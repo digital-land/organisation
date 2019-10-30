910c95a0224b7d5a1120e8b29059cb776d764d56
Paul Downey <psd@whatfettle.com>
2019-10-30 12:00:37 +0000

    Added tags for sections

--- render.py
+++ render.py
@@ -3,20 +3,35 @@
 import os
 import jinja2
 import csv
+from collections import OrderedDict
 
 loader = jinja2.FileSystemLoader(searchpath="./templates")
 env = jinja2.Environment(loader=loader)
 index = env.get_template("index.html")
 item = env.get_template("organisation.html")
 
-organisations = []
+
+tags = OrderedDict()
+for o in csv.DictReader(open("tag.csv")):
+    o["organisations"] = []
+    tags[o["tag"]] = o
+
+
 for o in csv.DictReader(open("organisation.csv")):
 
-    if o["opendatacommunities"]:
-        organisations.append(o)
+    o["path-segments"] = list(filter(None, o["organisation"].split(":")))
+    prefix = o["prefix"] = o["path-segments"][0]
 
-        o["path_segments"] = list(filter(None, o["organisation"].split(":")))
-        o["path"] = "/".join(o["path_segments"])
+    o.setdefault("tags", [])
+    o["tags"].append(prefix)
+
+    if (prefix in tags) and o["opendatacommunities"]:
+        tags[prefix]["organisations"].append(o)
+
+
+for tag in tags:
+    for o in tags[tag]["organisations"]:
+        o["path"] = "/".join(o["path-segments"])
 
         if o["path"] and not os.path.exists(o["path"]):
             os.makedirs(o["path"])
@@ -24,6 +39,5 @@ for o in csv.DictReader(open("organisation.csv")):
         with open(o["path"] + "/" + "index.html", "w") as f:
             f.write(item.render(organisation=o))
 
-
 with open("index.html", "w") as f:
-    f.write(index.render(organisations=organisations))
+    f.write(index.render(tags=tags))
