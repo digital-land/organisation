a7f519b2b21af9618225d01907dea977f6c80529
Colm Britton <colmjude@gmail.com>
2020-04-02 11:19:19 +0100

    Use hub identifiers if available

--- render_hub_page.py
+++ render_hub_page.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 organisation_csv = os.environ.get("organisation_csv", "https://raw.githubusercontent.com/digital-land/organisation-collection/master/collection/organisation.csv")
 organisation_tag_csv = os.environ.get("organisation_tag_csv", "https://raw.githubusercontent.com/digital-land/organisation-collection/master/data/tag.csv")
+hub_csv = "https://raw.githubusercontent.com/digital-land/organisation-collection/master/data/hub.csv"
 docs = "docs/"
 today = datetime.utcnow().isoformat()[:10]
 
@@ -45,7 +46,8 @@ def mapAHub(hub, data):
     return {
         'local-authority': [x['local-authority-name'] for x in data if x['hub'] == hub],
         'lrf': list(set([x['lrf-area'] for x in data if x['hub'] == hub]))[0],
-        'region': list(set([x['region'] for x in data if x['hub'] == hub]))[0]
+        'region': list(set([x['region'] for x in data if x['hub'] == hub]))[0],
+        'id': list(set([x['hub_id'] for x in data if x['hub'] == hub]))[0]
     }
 
 def mapHubData(data):
@@ -56,16 +58,44 @@ def mapHubData(data):
     return mapped
 
 
+def add_hub_identifiers(data, hub_ids):
+    no_match = []
+    ids = {x['name']: x['hub'] for x in hub_ids}
+
+    for row in data:
+        if row['hub'] in ids.keys():
+            row['hub_id'] = ids[row['hub']]
+        else:
+            row['hub_id'] = ""
+            no_match.append(row["hub"])
+
+    # from Rishi list not matched           
+    # print('\n'.join(sorted(list(set(no_match)))))
+    # print("====================")
+    # from psd list not matched
+    names = {hub:name for name,hub in ids.items()}
+    matched_names = [names[x['hub_id']] for x in data if x['hub_id'] is not '']
+    hub_csv_not_matched = [hub['name'] for hub in hub_ids if hub['name'] not in list(set(matched_names))]
+    # print('\n'.join(hub_csv_not_matched))
+
+
 def getHubData():
     data = pd.read_csv("./data/hubs.csv", sep=",")
     json_data = json.loads(data.to_json(orient='records'))
     councils = [x['local-authority-name'] for x in json_data]
+
+    # get hub identifiers
+    hId = pd.read_csv(hub_csv, sep=",")
+    jhId = json.loads(hId.to_json(orient='records'))
+    add_hub_identifiers(json_data, jhId)
+
     return councils, mapHubData(json_data)
 
 
 loader = jinja2.FileSystemLoader(searchpath="./templates")
 env = jinja2.Environment(loader=loader)
 hub_template = env.get_template("hub.html")
+
 
 # sort hub data by region
 councils, data = getHubData()
